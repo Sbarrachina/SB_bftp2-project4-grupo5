6900e6d0b3252eed906232a913f9c62a6acd8cd8
SMoliner <komissura@gmail.com>
2021-12-14 10:45:48 +0100

    creando pagina

---
+++ src/main/java/org/factoriaf5/bftp2project4grupo5/Bftp2Project4Grupo5Application.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.bftp2project4grupo5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bftp2Project4Grupo5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bftp2Project4Grupo5Application.class, args);
+    }
+
+}
