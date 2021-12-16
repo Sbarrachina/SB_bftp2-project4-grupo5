6e9a1b02c40e2879bfd3878839eea7225c50c850
joanalr1968 <joanalr@yahoo.com>
2021-12-16 14:34:21 +0100

    gamecontrollercarpeta agregar

--- src/main/java/repositories/SampleDataLoader.java
+++ src/main/java/repositories/SampleDataLoader.java
@@ -1,4 +1,32 @@
 package repositories;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Component
 public class SampleDataLoader {
+
+    private LegacyGameRepository legacyGameRepository;
+
+    @Autowired
+    public SampleDataLoader(LegacyGameRepository legacyGameRepository) {
+        this.legacyGameRepository = legacyGameRepository;
+    }
+
+    @PostConstruct
+    public void loadSampleData() {
+        legacyGameRepository.saveAll(List.of(
+                new Game("Una habitación propia", "Virginia Woolf", "Essay"),
+                new Game("Un mago de Terramar", "Ursula K. Leguin", "Fantasy"),
+                new Game("Los desposeídos", "Ursula K. Leguin", "Fantasy"),
+                new Game("Lean Software Development", "Mary Poppendieck", "Software"),
+                new Game("Women, Race and Class", "Angela Y. Davis", "Essay"),
+                new Game("Object Design", "Rebecca Wirfs-Brock", "Software")
+        ));
+    }
 }
+
+
